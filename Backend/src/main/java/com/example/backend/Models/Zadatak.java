27df8e39b2e6e5e377566a48e1044a304ff18463
franpavlovi <fljubuski@gmail.com>
2024-06-10 20:43:22 +0200

    ZadatakModel

---
+++ Backend/src/main/java/com/example/backend/Models/Zadatak.java
@@ -0,0 +1,55 @@
+package com.example.backend.Models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Email
+public class Zadatak {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Polje naziv je obvezno")
+    private String naziv;
+
+    private boolean status;
+
+
+    public Zadatak(Long id, String naziv, boolean status) {
+        this.id = id;
+        this.naziv = naziv;
+        this.status = status;
+    }
+
+    public Zadatak() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
