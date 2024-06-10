bde77011690ea7109211ef54d57f924d4b1e16d1
franpavlovi <fljubuski@gmail.com>
2024-06-10 20:08:25 +0200

    KorisnikRepository

---
+++ Backend/src/main/java/com/example/backend/Repositories/KorisnikRepository.java
@@ -0,0 +1,17 @@
+package com.example.backend.Repositories;
+
+
+import com.example.backend.Models.Korisnik;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface KorisnikRepository extends JpaRepository<Korisnik, Long> {
+
+        Korisnik findByEmail(String email);
+
+        boolean existsByEmail(String email);
+        
+}
