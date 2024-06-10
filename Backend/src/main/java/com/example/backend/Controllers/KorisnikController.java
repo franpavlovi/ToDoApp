c14ce9c8a2b046bda4025d9d46b080f9cae73a4d
franpavlovi <fljubuski@gmail.com>
2024-06-10 20:39:39 +0200

    KorisnikController

---
+++ Backend/src/main/java/com/example/backend/Controllers/KorisnikController.java
@@ -0,0 +1,50 @@
+package com.example.backend.Controllers;
+
+import com.example.backend.Models.Korisnik;
+import com.example.backend.Services.KorisnikService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/api/korisnik")
+public class KorisnikController {
+
+    @Autowired
+    KorisnikService korisnikService;
+
+
+    @GetMapping("/listaKorisnika")
+    public ResponseEntity<List<Korisnik>> getAllKorisnici(){
+
+       List<Korisnik> korisnici = korisnikService.getAllKorisnici();
+       return ResponseEntity.ok(korisnici);
+    }
+
+    @PostMapping
+    public ResponseEntity<Korisnik> createKorisnik(@RequestBody Korisnik korisnik) {
+
+        Korisnik noviKorisnik = korisnikService.createKorisnik(korisnik);
+        return ResponseEntity.ok(noviKorisnik);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteKorisnik(@PathVariable Long id) {
+
+        korisnikService.deleteKorisnik(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Korisnik> updateKorisnik(@PathVariable Long id, @RequestBody Korisnik noviKorisnik) {
+
+        Korisnik azuriraniKorisnik = korisnikService.updateKorisnik(id, noviKorisnik);
+        return ResponseEntity.ok(azuriraniKorisnik);
+    }
+
+
+}
