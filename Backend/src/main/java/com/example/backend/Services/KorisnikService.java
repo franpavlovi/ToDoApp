5ac88474eb6a9d4dc5116da9526e00698503fed2
franpavlovi <fljubuski@gmail.com>
2024-06-10 22:37:08 +0200

    prijava i registracija methods

--- Backend/src/main/java/com/example/backend/Services/KorisnikService.java
+++ Backend/src/main/java/com/example/backend/Services/KorisnikService.java
@@ -4,6 +4,7 @@ import com.example.backend.Models.Korisnik;
 import com.example.backend.Repositories.KorisnikRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -51,6 +52,43 @@ public class KorisnikService {
         korisnik.setPotvrdaLozinke(noviKorisnik.getPotvrdaLozinke());
 
         return korisnikRepository.save(korisnik);
+
+    }
+
+
+    public Korisnik registracija(Korisnik korisnik){
+
+        if(korisnikRepository.existsByEmail(korisnik.getEmail())){
+
+            throw new RuntimeException("Korisnik sa istom email adresom već postoji");
+        }
+
+        if(!korisnik.podudaranjeLozinki()){
+
+            throw new RuntimeException("Lozinke se ne podudaraju");
+
+        }else{
+
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            korisnik.setLozinka(encoder.encode(korisnik.getLozinka()));
+        }
+
+        return korisnikRepository.save(korisnik);
+
+    }
+
+    public void prijava(String email, String lozinka){
+
+        Korisnik korisnik = korisnikRepository.findByEmail(email);
+
+        if (korisnik == null) {
+            throw new RuntimeException("Neuspješna prijava. Provjerite email adresu i lozinku.");
+        }
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if (!encoder.matches(lozinka, korisnik.getLozinka())) {
+            throw new RuntimeException("Neuspješna prijava. Provjerite email adresu i lozinku.");
+        }
 
     }
 
