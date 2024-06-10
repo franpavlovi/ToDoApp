880705e22363bd0ef3d6720bbffa923fbe6541cc
franpavlovi <fljubuski@gmail.com>
2024-06-10 22:43:07 +0200

    AuthController

---
+++ Backend/src/main/java/com/example/backend/Controllers/AuthController.java
@@ -0,0 +1,44 @@
+package com.example.backend.Controllers;
+
+import com.example.backend.Models.Korisnik;
+import com.example.backend.Services.KorisnikService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("/api/auth")
+public class AuthController {
+
+    @Autowired
+    KorisnikService korisnikService;
+
+    @PostMapping("/registracija")
+    public ResponseEntity<String> registracijaKorisnika(@RequestBody Korisnik korisnik){
+
+        try{
+        korisnikService.registracija(korisnik);
+        return new ResponseEntity<>("Registracija uspješna", HttpStatus.CREATED);
+        }
+        catch (RuntimeException e){
+
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+
+        }
+
+    }
+
+    @PostMapping("/prijava")
+    public ResponseEntity<String> prijavaKorisnika(@RequestBody Korisnik korisnik) {
+        try {
+            korisnikService.prijava(korisnik.getEmail(), korisnik.getLozinka());
+            return new ResponseEntity<>("Prijava uspješna", HttpStatus.OK);
+        }
+        catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
